1afa773d60a41afaf088731413b43355a057db38
piyushchouhan <chouhanpiyush142@outlook.com>
2022-11-29 03:31:20 +0530

    Completion of Factory Design

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -94,36 +94,36 @@ namespace DotNetDesignPatternDemos
 
     // ---------------------------Factory Method----------------------------------------- //
 
-    public class Point
-    {
-        private readonly double x;
-        private readonly double y;
-        //Factory Method Design Pattern
+    //public class Point
+    //{
+    //    private readonly double x;
+    //    private readonly double y;
+    //    //Factory Method Design Pattern
 
-        //public static Point NewCartesianPoint(double x, double y)
-        //{
-        //    return new Point(x, y);
-        //}
+    //    //public static Point NewCartesianPoint(double x, double y)
+    //    //{
+    //    //    return new Point(x, y);
+    //    //}
 
-        //public static Point NewPolarPoint(double rho, double theta)
-        //{
-        //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        //}
+    //    //public static Point NewPolarPoint(double rho, double theta)
+    //    //{
+    //    //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    //    //}
 
-        public Point(double x, double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
+    //    public Point(double x, double y)
+    //    {
+    //        this.x = x;
+    //        this.y = y;
+    //    }
 
-        public override string ToString()
-        {
-            var sB = new StringBuilder();
-            sB.AppendLine("X: " + x);
-            sB.AppendLine("Y: " + y);
-            return sB.ToString();
-        }
-    }
+    //    public override string ToString()
+    //    {
+    //        var sB = new StringBuilder();
+    //        sB.AppendLine("X: " + x);
+    //        sB.AppendLine("Y: " + y);
+    //        return sB.ToString();
+    //    }
+    //}
 
 
     // ------------------ Asynchronous Factory Methods -------------------//
@@ -154,18 +154,18 @@ namespace DotNetDesignPatternDemos
     //We will face bunch of problems as we have the point constructor private.
 
 
-    public class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
+    //public class PointFactory
+    //{
+    //    public static Point NewCartesianPoint(double x, double y)
+    //    {
+    //        return new Point(x, y);
+    //    }
 
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
+    //    public static Point NewPolarPoint(double rho, double theta)
+    //    {
+    //        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    //    }
+    //}
 
     //--------------------------- Object Tracking and Bulk Replacement ---------------------------//
 
@@ -225,7 +225,7 @@ namespace DotNetDesignPatternDemos
 
         public ITheme createThemeImpl(bool dark)
         {
-            return dark ? new DarkTheme() : new LightTheme(); 
+            return dark ? new DarkTheme() : new LightTheme();
         }
 
         public Ref<ITheme> CreateTheme(bool dark)
@@ -239,7 +239,8 @@ namespace DotNetDesignPatternDemos
         {
             foreach (var weakRef in themes)
             {
-                if(weakRef.TryGetTarget(out var reference)) { 
+                if (weakRef.TryGetTarget(out var reference))
+                {
                     reference.Value = createThemeImpl(dark);
                 }
             }
@@ -255,6 +256,167 @@ namespace DotNetDesignPatternDemos
         public Ref(T value)
         {
             Value = value; 
+        }
+    }
+
+
+    //---------------------Inner Factory-----------------//
+
+    public class Point
+    {
+        private readonly double x;
+        private readonly double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            var sB = new StringBuilder();
+            sB.AppendLine("X: " + x);
+            sB.AppendLine("Y: " + y);
+            return sB.ToString();
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+
+
+    //--------------------Abstract Factory-----------------//
+
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("Tea with milk is delicious you should try it.");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("Coffee can keep you awake for hours");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Add Tea leaves with {amount} ml Milk and add sugar");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Add Beans, boil water and add {amount} of milk then add cream and sugar");
+            return new Coffee();
+        }
+    }
+
+    //public class HotDrinksMachine
+    //{
+
+    //    //Here we are voilating the open closed principle. What if you want to introduce a new drink in future? You will have to change the Enum
+    //    // values and that is the voliation of the openclosed principle. To work around this issue we use reflection.
+    //    public enum AvailableDrinks
+    //    {
+    //        Coffee,
+    //        Tea             
+    //    }
+
+    //    private Dictionary<AvailableDrinks, IHotDrinkFactory> factories = new Dictionary<AvailableDrinks, IHotDrinkFactory>();
+
+    //    public HotDrinksMachine()
+    //    {
+    //        foreach (AvailableDrinks drink in Enum.GetValues(typeof(AvailableDrinks)))
+    //        {
+    //            var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType("DotNetDesignPatternDemos."
+    //                + Enum.GetName(typeof(AvailableDrinks), drink) + "Factory"));
+    //            factories.Add(drink, factory);
+    //        }
+    //    }
+
+    //    public IHotDrink MakeDrink(AvailableDrinks drink, int amount){
+    //        return factories[drink].Prepare(amount);
+    //    }
+    //}
+
+    //--------------------Abstract Factory and OCP-----------------//
+
+    public class HotDrinkMachine
+    {
+        List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
+        public HotDrinkMachine()
+        {
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(t.Name.Replace("Factory", String.Empty), (IHotDrinkFactory)Activator.CreateInstance(t)));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink
+        {
+            get
+            {
+                WriteLine("Available Drinks");
+                for (int index = 0; index < factories.Count; index++)
+                {
+                    Tuple<string, IHotDrinkFactory>? tuple = factories[index];
+                    WriteLine($"{index}: {tuple.Item1}");
+                }
+                while (true)
+                {
+                    string s;
+                    if ((s = Console.ReadLine()) != null
+                        && int.TryParse(s, out int i)
+                        && i >= 0
+                        && i < factories.Count()
+                        )
+                    {
+                        WriteLine("Specify Amount: ");
+                        s = ReadLine();
+                        if (s != null
+                            && int.TryParse(s, out int amount)
+                            && amount > 0)
+                        {
+                            return factories[i].Item2.Prepare(amount);
+                        }
+                    }
+                    WriteLine("Incorrect Selection, try again!");
+                }
+            }
         }
     }
 
@@ -317,23 +479,43 @@ namespace DotNetDesignPatternDemos
 
             //--------------------------- Object Tracking and Bulk Replacement ---------------------------//
 
-            var factory = new TrackingThemeFactory();
-            var theme1 = factory.CreateTheme(true);
-            var theme2 = factory.CreateTheme(false);
+            //var factory = new TrackingThemeFactory();
+            //var theme1 = factory.CreateTheme(true);
+            //var theme2 = factory.CreateTheme(false);
 
-            WriteLine(factory.FactoryInfo);
+            //WriteLine(factory.FactoryInfo);
 
-            var factory2 = new ReplaceableThemeFactory();
-            var theme3 = factory2.CreateTheme(true);
-            var theme4 = factory2.CreateTheme(false);
+            //var factory2 = new ReplaceableThemeFactory();
+            //var theme3 = factory2.CreateTheme(true);
+            //var theme4 = factory2.CreateTheme(false);
 
-            WriteLine(theme3.Value.BgrColor);
-            WriteLine(theme4.Value.BgrColor);
+            //WriteLine(theme3.Value.BgrColor);
+            //WriteLine(theme4.Value.BgrColor);
 
-            factory2.ReplaceTheme(true);
+            //factory2.ReplaceTheme(true);
 
-            WriteLine(theme3.Value.BgrColor);
-            WriteLine(theme4.Value.BgrColor);
+            //WriteLine(theme3.Value.BgrColor);
+            //WriteLine(theme4.Value.BgrColor);
+
+            //---------------------Inner Factory-----------------//
+
+            //var point = Point.Factory.NewCartesianPoint(3, 5);
+            //WriteLine(point);
+
+
+            //--------------------Abstract Factory-----------------//
+            //var hotDrink = new HotDrinksMachine();
+            //var Tea = hotDrink.MakeDrink(HotDrinksMachine.AvailableDrinks.Tea, 200);
+            ////var Coffee = hotDrink.MakeDrink(HotDrinksMachine.AvailableDrinks.Coffee, 100);
+
+            //Tea.Consume();
+            //Coffee.Consume();
+
+            //--------------------Abstract Factory and OCP-----------------//
+
+            var hotdrinkMachine = new HotDrinkMachine();
+            var drink = hotdrinkMachine.MakeDrink;
+            drink.Consume();
         }
     }
 }
